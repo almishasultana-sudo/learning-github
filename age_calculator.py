40a162da1b7de36bc65f58fc8cc5afe10d59f7d3
almishasultana-sudo <almishasultana@gmail.com>
2026-04-08 18:18:22 +0530

    Mini Project: Age Calculator

---
+++ age_calculator.py
@@ -0,0 +1,14 @@
+name = input("Enter your name: ")
+birth_year = int(input("Enter your birth year: "))
+
+current_year = 2026
+
+age = current_year - birth_year
+
+print("Hello", name)
+print("You are", age, "years old.")
+
+if age >= 18:
+    print("You are an adult.")
+else:
+    print("You are a minor.")
