6a2c28c7ffcbd095ba152bca19429bdd5b76956a
almishasultana-sudo <almishasultana@gmail.com>
2026-04-08 18:24:39 +0530

    Day 4: Learned Python loops

---
+++ day4_loops.py
@@ -0,0 +1,10 @@
+for i in range(5):
+    print("I am becoming a Python developer 🚀")
+
+for i in range(1, 6):
+    print("Step", i)
+
+name = input("Enter your name: ")
+
+for i in range(3):
+    print("Welcome", name)
